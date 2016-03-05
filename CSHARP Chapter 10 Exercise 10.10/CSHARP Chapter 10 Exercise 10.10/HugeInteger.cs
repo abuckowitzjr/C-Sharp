0c3f94796626e36299652cfd41aead3385caef57
abuckowitzjr <abuckowitzjr_git@yahoo.com>
2016-03-05 13:56:41 -0600

    Final Changes made to Program.cs and HugeInteger Class for proper functionality

--- CSHARP Chapter 10 Exercise 10.10/CSHARP Chapter 10 Exercise 10.10/HugeInteger.cs
+++ CSHARP Chapter 10 Exercise 10.10/CSHARP Chapter 10 Exercise 10.10/HugeInteger.cs
@@ -1,94 +1,125 @@
 ﻿using System;
 using System.Numerics;
+using System.Text;
 
 public class HugeInteger
 {
+    BigInteger num1 = 0; //For storing BigInteger values for use in the methods
+    BigInteger num2 = 0;
 
-    bool isEqualTo = true;
-
-    public BigInteger Input(string number)
+    public void Input(string input1, string input2)
     {
-        BigInteger bi = BigInteger.Parse(number);
-        return bi;
+        var chars1 = input1.ToCharArray(); //This could have been done without adding the strings to Char Array, but the book insisted I do so in this method so... yeah...
+        var chars2 = input2.ToCharArray(); //
+        string n1 = new String(chars1); //Creates new strings based on chars in arrays
+        string n2 = new String(chars2); //
+        num1 = BigInteger.Parse(n1); //Parses BigInteger numbers from new strings and stores in class variables
+        num2 = BigInteger.Parse(n2); //
     }
-    public string ToString(BigInteger value)
+
+    public void Clear() //Clears values in Biginteger variables when called from Main
+    {
+        num1 = 0;
+        num2 = 0;
+    }
+
+    public string ToString(BigInteger value) //Converts any BigInteger value passed to it to string for use in Main for Console.Write
     {
         string output = value.ToString();
         return output;
     }
-    public string Add(BigInteger number1, BigInteger number2)
+
+    public string ToString(Decimal value) //Converts any Decimal value passed to it to string for use in Main for Console.Write
     {
-        BigInteger sum = number1 + number2;
-        return ToString(sum);
+        string output = value.ToString();
+        return output;
     }
-    public string Subtract(BigInteger number1, BigInteger number2)
+
+    public BigInteger Add() //Adds numbers
     {
-        BigInteger diff = 0;
-        if (number1 > number2)
-            diff = number1 - number2;
-        else
-            diff = number2 - number1;
-        return ToString(diff);
+        BigInteger sum = num1 + num2;
+        return sum;
     }
-    public bool IsEqualTo()
+    public BigInteger Subtract() //Subtracts numbers
+    {
+        BigInteger diff = num1 - num2;
+        return diff;
+    }
+    public bool IsEqualTo() //**Comparisons of numbers returning true or false
     {
         bool result = false;
-
+        if (num1 == num2)
+            result = true;
         return result;
     }
-    public bool IsNotEqualTo()
+    public bool IsNotEqualTo() //**
     {
         bool result = false;
-
+        if (num1 != num2)
+            result = true;
         return result;
     }
-    public bool IsGreaterThan()
+    public bool IsGreaterThan() //**
     {
         bool result = false;
-
+        if (num1 > num2)
+            result = true;
         return result;
     }
-    public bool IsLessThan()
+    public bool IsLessThan() //**
     {
         bool result = false;
-
+        if (num1 < num2)
+            result = true;
         return result;
     }
-    public bool IsGreaterThanOrEqualTo()
+    public bool IsGreaterThanOrEqualTo() //**
     {
         bool result = false;
-
+        if (num1 >= num2)
+            result = true;
         return result;
     }
-    public bool IsLessThanOrEqualTo()
+    public bool IsLessThanOrEqualTo() //** 
     {
         bool result = false;
-
+        if (num1 <= num2)
+            result = true;
         return result;
     }
-    public bool IsZero()
+    public bool IsZero(string choice) //Checks if numbers passed in are zero
     {
         bool result = false;
-
+        if (choice == "1")
+        {
+            if (num1 == 0)
+                result = true;
+        }
+        if (choice == "2")
+        {
+            if (num2 == 0)
+                result = true;
+        }
         return result;
     }
-    public int Multiply()
+    public BigInteger Multiply() //Returns product of numbers
     {
-        int result = 0;
-
-        return result;
+        BigInteger product = num1 * num2;
+        return product;
     }
-    public int Divide()
+    public Decimal Divide() //Returns quotient of numbers
     {
-        int result = 0;
-
-        return result;
+        Decimal decnum1 = Convert.ToDecimal(ToString(num1));
+        Decimal decnum2 = Convert.ToDecimal(ToString(num2));
+        Decimal quotient = Math.Round(decnum1 / decnum2, 3);
+        return quotient;
     }
-    public int Remainder()
+    public Decimal Remainder() //Returns remainder of numbers
     {
-        int result = 0;
-
-        return result;
+        String strnum1 = ToString(num1);
+        String strnum2 = ToString(num2);
+        Decimal remainder = Math.Round(Convert.ToDecimal(strnum1) % Convert.ToDecimal(strnum2), 3);
+        return remainder;
     }
 
 
--- CSHARP Chapter 10 Exercise 10.10/CSHARP Chapter 10 Exercise 10.10/Program.cs
+++ CSHARP Chapter 10 Exercise 10.10/CSHARP Chapter 10 Exercise 10.10/Program.cs
@@ -11,52 +11,79 @@ namespace CSHARP_Chapter_10_Exercise_10._10
         static void Main()
         {
             HugeInteger hugeInteger = new HugeInteger();
-            int n = 0;
-            int c = 0;
             string number1 = "";
             string number2 = "";
+            int n = 0;
+            int c = 0;
             while (c == 0)
             {
                 while (n == 0)
                 {
-                    Console.Write("Enter the first number, no more than 40 digits in length: ");
+                    Console.Write("Enter the first number, no more than 40 digits in length: "); 
                     number1 = Console.ReadLine();
-                    if (number1.Length > 40)
+                    if (number1.Length > 40) //Checks to ensure number is not larger than 40 digits
                     {
                         Console.WriteLine("Number entered has more than 40 digits, try again");
                         Console.WriteLine("");
                     }
                     else
-                        n = 1;
+                    {
+                        n = 1; //Continues when number is within acceptable range
+                    }
                 }
                 while (n == 1)
                 {
-                    Console.Write("Enter the second number, no more than 40 digits in length: ");
+                    Console.Write("Enter the second number, no more than 40 digits in length: "); 
                     number2 = Console.ReadLine();
-                    if (number2.Length > 40)
+                    if (number2.Length > 40) //Checks to ensure number is not larger than 40 digits
                     {
                         Console.WriteLine("Number entered has more than 40 digits, try again");
                         Console.WriteLine("");
                     }
                     else
-                        Console.WriteLine("");
+                    {
+                        Console.WriteLine(""); //Continues when number is within acceptable range
                         n = 2;
+                    }
                 }
                 Console.WriteLine("You have entered the following numbers:");
                 Console.WriteLine("First Number: {0}", number1);
                 Console.WriteLine("Second Number: {0}", number2);
-                Console.Write("Enter 1 if you accept these numbers, or 2 to enter new ones: ");
+                Console.WriteLine("Enter 1 if you accept these numbers, or 2 to enter new ones.");
+                Console.WriteLine("Keep in mind, all calculations will be done in order.");
+                Console.Write("Example (First Number - Second Number = ?) or (Is First Number > Second Number): ");
                 if (Console.ReadLine() == "1")
                 {
                     c = 1;
+                    hugeInteger.Input(number1, number2); //From here down, uses HugeInteger Class heavily to calculate and compare numbers
                     Console.WriteLine("");
                     Console.WriteLine("The following information pertains to the two numbers you entered:");
-                    Console.WriteLine("Sum of the numbers: {0}", hugeInteger.Add(hugeInteger.Input(number1), hugeInteger.Input(number2)));
-                    Console.WriteLine("Difference of the numbers: {0}", hugeInteger.Subtract(hugeInteger.Input(number1), hugeInteger.Input(number2)));
+                    Console.WriteLine("The sum of the numbers: {0}", hugeInteger.ToString(hugeInteger.Add()));
+                    Console.WriteLine("The difference of the numbers: {0}", hugeInteger.ToString(hugeInteger.Subtract()));
+                    Console.WriteLine("The numbers are equal: {0}", hugeInteger.IsEqualTo());
+                    Console.WriteLine("The numbers are not equal: {0}", hugeInteger.IsNotEqualTo());
+                    Console.WriteLine("The first number is greater than the second: {0}", hugeInteger.IsGreaterThan());
+                    Console.WriteLine("The first number is less than the second: {0}", hugeInteger.IsLessThan());
+                    Console.WriteLine("The first number is greater than or equal to the second: {0}", hugeInteger.IsGreaterThanOrEqualTo());
+                    Console.WriteLine("The first number is less than or equal to the second: {0}", hugeInteger.IsLessThanOrEqualTo());
+                    Console.WriteLine("The first number is zero: {0}", hugeInteger.IsZero("1"));
+                    Console.WriteLine("The second number is zero: {0}", hugeInteger.IsZero("2"));
+                    Console.WriteLine("The product of the numbers: {0}", hugeInteger.ToString(hugeInteger.Multiply()));
+                    if (number2 == "0")
+                        Console.WriteLine("The quotient of the numbers: Unable to process, you cannot divide by zero."); //Ensures dividing by 0 does not occur, as it is not possible
+                    else
+                        Console.WriteLine("The quotient of the numbers: {0}", hugeInteger.ToString(hugeInteger.Divide()));
+                    if (number2 == "0")
+                        Console.WriteLine("The remainder of the numbers: Unable to process, you cannot divide by zero."); //Ensures dividing by 0 does not occur, as it is not possible
+                    else
+                        Console.WriteLine("The remainder of the numbers: {0}", hugeInteger.ToString(hugeInteger.Remainder()));
                 }
                 else
+                {
+                    hugeInteger.Clear(); //Clears values to enter new numbers
                     Console.WriteLine("");
-                n = 0;
+                    n = 0;
+                }  
             }
         }
     }
